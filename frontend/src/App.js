a043b1253e2e7ff72fdabb073ec05573ae1957a5
tillandert <116759551+tillandert@users.noreply.github.com>
2024-03-22 01:19:42 -0400

    frontend basic design and stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(URL.createObjectURL(file));
+    // Here you can connect with your models trained in .jpynb
+    // and perform any necessary processing on the uploaded image
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Fruit Identifier Image Upload</h1>
+        <b>Created by Kushal Gaddam, Justin Galin, Helena He, Cathy Quan, and Taylor Tillander</b>
+        <div>
+        <input type="file" accept="image/*" onChange={handleImageUpload} />
+        </div>
+        {selectedImage && (
+          <img src={selectedImage} alt="Uploaded" className="uploaded-image" />
+        )}
       </header>
     </div>
   );
